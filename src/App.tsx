dd9e53cf96282814018dd8ab155fd7e21ab2e289
Arne Hassel <arne.hassel@capgemini.com>
2024-02-14 12:02:44 +0100

    Added reveal.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Reveal from 'reveal.js';
+import {useLayoutEffect} from "react";
+import "reveal.js/dist/reset.css";
+import "reveal.js/dist/reveal.css";
+import "reveal.js/dist/theme/black.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const deck = new Reveal({
+    hash: true
+});
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+    useLayoutEffect(() => {
+        deck.initialize();
+    }, []);
+
+    return (
+        <div className="reveal">
+            <div className="slides">
+                <section>Slide 1</section>
+                <section>Slide 2</section>
+            </div>
+        </div>
+    )
 }
-
-export default App
