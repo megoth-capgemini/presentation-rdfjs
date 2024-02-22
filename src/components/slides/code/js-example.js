d64358d1f91cc2a1fc864ca3f928da4b3c7cbddc
Arne Hassel <arne.hassel@capgemini.com>
2024-02-22 16:37:55 +0100

    Added notes to all slides

--- src/components/slides/code/js-example.js
+++ src/components/slides/code/js-example.js
@@ -1,19 +1,22 @@
-var variable = null
+var variable
+variable = null
 variable = undefined
 
 const constant = 42
-constant = "Something" // not allowed
-let anotherVariable = 'Something else' // initiate flame wars
+constant = "Something"
+let anotherVariable = 'Something else'
 anotherVariable = `Template string that allow interpolation: ${constant}`
 
-var object = {} // new Object()
-var array = [] // new Array()
-var set = new Set()
+const object = {foo: 42}
+object.bar = "Thank you for the fish"
+const array = [42, 42, "Thank you for all the fish"]
+array[0] = 1337
+const set = new Set([42, 42, "Thank you for all the fish"])
 
-function myFunction (variable) {
+function myFunction(variable) {
     return variable + 1
 }
-var anonymousFunction = function (variable) {
+const anonymousFunction = function (variable) {
     return variable + 1
 }
-var arrowFunction = (variable) => variable + 1
+const arrowFunction = (variable) => variable + 1
--- src/components/slides/index.tsx
+++ src/components/slides/index.tsx
@@ -1,21 +1,26 @@
 import Intro from "./00-intro.mdx";
 import Overview from "./10-overview.mdx";
-import RdfIntro from "./20-rdf-intro.mdx";
-import RdfSemantics from "./21-semantics.mdx";
-import RdfTriples from "./22-triples.mdx";
-import RdfModels from "./23-models.mdx";
-import RdfSerializations from "./24-serializations.mdx";
-import RdfVocabularies from "./25-vocabularies.mdx";
-import RdfTurtle from "./26-turtle.mdx";
-import JavaScriptIntro from "./30-javascript-intro.mdx";
-import JavaScriptSyntax from "./31-syntax.mdx";
-import JavaScriptTypes from "./32-typescript.mdx";
-import ToolsIntro from "./40-tools-intro.mdx";
-import ToolsRdflib from "./41-rdflib.mdx";
-import ToolsLDO from "./42-ldo.mdx";
-import ToolsShapes from "./43-ldo-cont.mdx";
-import ConclusionIntro from "./50-conclusion.mdx";
-import ConclusionThanks from "./51-thanks.mdx";
+import Motivation from "./20-motivation.mdx";
+import MotivationChallenge from "./21-challenge.mdx";
+import RdfIntro from "./30-rdf-intro.mdx";
+import RdfSemantics from "./31-semantics.mdx";
+import RdfTriples from "./32-triples.mdx";
+import RdfModels from "./33-models.mdx";
+import RdfVocabularies from "./34-vocabularies.mdx";
+import RdfSerializations from "./35-serializations.mdx";
+import RdfTurtle from "./36-turtle.mdx";
+import RdfJsonLD from "./37-jsonld.mdx";
+import RdfJsonLDPart2 from "./38-jsonld-cont.mdx";
+import JavaScriptIntro from "./40-javascript-intro.mdx";
+import JavaScriptSyntax from "./41-syntax.mdx";
+import JavaScriptTypes from "./42-typescript.mdx";
+import ToolsIntro from "./50-tools-intro.mdx";
+import ToolsRdflib from "./51-rdflib.mdx";
+import ToolsLDO from "./52-ldo.mdx";
+import ToolsShapes from "./53-ldo-cont.mdx";
+import ConclusionIntro from "./60-conclusion.mdx";
+import ConclusionMore from "./61-more.mdx";
+import ConclusionThanks from "./62-thanks.mdx";
 import "./theme.css";
 
 export default function Slides() {
@@ -23,13 +28,19 @@ export default function Slides() {
         <section id="intro"><Intro/></section>
         <section id="overview"><Overview/></section>
         <section>
+            <section id="motivation"><Motivation/></section>
+            <section id="motivation-challenge"><MotivationChallenge/></section>
+        </section>
+        <section>
             <section id="rdf"><RdfIntro/></section>
-            <section id="rdf-semantic"><RdfSemantics/></section>
+            <section id="rdf-semantics"><RdfSemantics/></section>
             <section id="rdf-triples"><RdfTriples/></section>
             <section id="rdf-models"><RdfModels/></section>
-            <section id="rdf-serializations"><RdfSerializations/></section>
             <section id="rdf-vocabularies"><RdfVocabularies/></section>
+            <section id="rdf-serializations"><RdfSerializations/></section>
             <section id="rdf-turtle"><RdfTurtle/></section>
+            <section id="rdf-jsonld"><RdfJsonLD/></section>
+            <section id="rdf-jsonld-part2"><RdfJsonLDPart2/></section>
         </section>
         <section>
             <section id="javascript"><JavaScriptIntro/></section>
@@ -44,6 +55,7 @@ export default function Slides() {
         </section>
         <section>
             <section id="conclusion"><ConclusionIntro/></section>
+            <section id="conclusion-more"><ConclusionMore/></section>
             <section id="conclusion-thanks"><ConclusionThanks/></section>
         </section>
     </div>
