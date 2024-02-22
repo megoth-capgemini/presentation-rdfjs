6df79df4cca82cfb18edf78b878a36541135c1d4
Arne Hassel <arne.hassel@capgemini.com>
2024-02-22 16:47:58 +0100

    Fixed some typos and improved texts in general

--- src/components/slides/code/js-example.js
+++ src/components/slides/code/js-example.js
@@ -16,7 +16,6 @@ const set = new Set([42, 42, "Thank you for all the fish"])
 function myFunction(variable) {
     return variable + 1
 }
-const anonymousFunction = function (variable) {
-    return variable + 1
-}
+const result = myFunction(42)
 const arrowFunction = (variable) => variable + 1
+const result2 = arrowFunction(1337)
